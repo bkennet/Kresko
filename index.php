469a029cc9aace6acf51e31a621dfaef83c2043b
Matthew <mcres@dhcp-hol-2723.redrover.cornell.edu>
2016-04-22 13:17:06 -0400

    html skeleton

--- index.php
+++ index.php
@@ -1,10 +1,41 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Kresko: Artisinal Crafts and Products</title>
-		<link rel='stylesheet' type='text/css' href='./css/main.css'>
-	</head>
-	<body>
+  <head>
+    <title>Kresko: Artisinal Crafts and Products</title>
+    <link rel='stylesheet' type='text/css' href='./css/main.css'>
+    <link rel='stylesheet' type='text/css' href='./css/bootstrap.css'>
+    <!-- <link rel='stylesheet' type='text/css' href='./css/bootstrap-theme.css'> -->
+  </head>
+  <body>
+    <div class='container-fluid page-wrapper'> <!-- This will wrap the entire page: allows us to use bootstrap rows and columns -->
 
-	</body>
+      <div class="row header"> <!-- Header: Logo, Title, Little Blurb, Navigation Bar -->
+        <div class="sign-in">
+          Sign-in/Sign-out
+        </div>
+
+        <div class="title">
+          <p>Kresko</p>
+        </div>
+
+        <div class="navigation">
+          <div class="col-md-3">
+            Vendors
+          </div>
+          <div class="col-md-3">
+            Items
+          </div>
+          <div class="col-md-3">
+            Other
+          </div>
+          <div class="col-md-3">
+            Misc
+          </div>
+        </div>
+      </div> <!-- end div header -->
+
+      <div class="row content">
+      </div>
+    </div>
+  </body>
 </html>
