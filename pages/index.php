9c58b21f640c921ffef6e72afdf8d0901e2c0a55
Matthew <mcres@dhcp-gs-2173.redrover.cornell.edu>
2016-05-01 15:28:47 -0400

    merged with Dan

--- pages/index.php
+++ pages/index.php
@@ -6,32 +6,43 @@
   <body>
     <div class='container-fluid page-wrapper'> <!-- This will wrap the entire page: allows us to use bootstrap rows and columns -->
 
-      <div class="row header"> <!-- Header: Logo, Title, Little Blurb, Navigation Bar -->
-        <div class="sign-in">
-          <a href="./log-in.php"> Sign-in/Sign-out </a>
+      <div class="header"> <!-- Header: Logo, Title, Little Blurb, Navigation Bar -->
+
+        <img id='homepicture' src="../images/misc/background.jpg" alt='hi'>
+
+        <div class="login">
+          <a href ="./log-in.php">LOGIN</a>
+          <a href = "cart.php">BAG</a>
         </div>
 
         <div class="title">
-          <p>Kresko</p>
+            <a id="brand" href ="index.php">KRESKO</a>
         </div>
 
         <div class="navigation">
-          <div class="col-md-3">
-            Vendors
+          <div class="col-md-4">
+            <a href ="artisans.php">ARTISANS</a>
           </div>
-          <div class="col-md-3">
-            Items
+          <div class="col-md-4">
+            <a href ="clothing.php">CLOTHING</a>
           </div>
-          <div class="col-md-3">
-            Other
-          </div>
-          <div class="col-md-3">
-            Misc
+          <div class="col-md-4">
+            <a href ="accessories.php">ACCESSORIES</a>
           </div>
         </div>
       </div> <!-- end div header -->
 
-      <div class="row content">
+      <div class="content">
+        <div id="box1"></div>
+        <div id="box2"></div>
+        <div id="box3"></div>
+        <div id="box4"></div>
+        <div id="box5"></div>
+        <div id="box6"></div>
+      </div>
+
+      <div class="footer">
+        hi
       </div>
     </div>
   </body>
