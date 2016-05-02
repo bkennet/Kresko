5ad7d11406a0e46b1ab19b845f8c89704c313494
Matthew <mcres@dhcp-gs-2173.redrover.cornell.edu>
2016-05-01 20:13:58 -0400

    category page added

---
+++ includes/navigation.php
@@ -0,0 +1,26 @@
+<img id='homepicture' src="../images/misc/background.jpg" alt='hi'>
+     
+<div class="header"> <!-- Header: Logo, Title, Little Blurb, Navigation Bar -->
+
+
+  <div class="login">
+    <a href ="./log-in.php">LOGIN</a>
+    <a href = "cart.php">BAG</a>
+  </div>
+
+  <div class="title">
+      <a id="brand" href ="index.php">KRESKO</a>
+  </div>
+
+  <div class="navigation">
+    <div class="col-md-4">
+      <a href ="artisans.php">ARTISANS</a>
+    </div>
+    <div class="col-md-4">
+      <a href ="clothing.php">CLOTHING</a>
+    </div>
+    <div class="col-md-4">
+      <a href ="accessories.php">ACCESSORIES</a>
+    </div>
+  </div>
+</div> <!-- end div header -->
