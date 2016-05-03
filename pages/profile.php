2470fec5b6e682f5ce78bbce3c077c4c37e66de4
bkennet <benjaminkennet@gmail.com>
2016-05-03 00:54:45 -0400

    a few more changes to profile

--- pages/profile.php
+++ pages/profile.php
@@ -18,9 +18,8 @@
 			<!-- Name pulled from database, userid, usertype and username are all thats stored in session.-->
             <h3 class="white">Artisan Name</h3>
 			<!-- Profile information viewable below, edit submission not functional yet. This displays name, description and image from the database-->
-			<textarea rows='4' wrap='virtual' class="edit-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
-			<label for="new-photo" class='white'>Modify vendor image:</label>
-			<input class="white" id="new-photo" type="file" name="newphoto"><br><br>
+			<textarea rows='4' wrap='virtual' class="edit-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea><br>
+			<span class='white'>Modify vendor image:</span> <input class="white" id="new-photo" type="file" name="newphoto"><br><br>
 			<!-- Button to submit changes to vendor photo and description. Name of vendor not editable. This is all the vendor should need to edit.
 			-->
 			<input type="submit" name="edit_profile" value="Save changes" >	
