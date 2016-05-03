70c596859e141fc9e4b2945b0507f6db7cc9d588
Dan Kim <Dank@Dans-MacBook-Pro.local>
2016-05-03 03:24:06 -0400

    working on footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,23 @@
 <div>
-	<h3>We're Social</h3>
+
+	<div id="contact">
+		<h3>Contact Us:<h3>
+		<p>email: abcd1234@gmail.com</p>
+		<p>Phone: 123-456-7890</p>
+	</div>
+
+	<div id="about">
+		<h3>Who we are:<h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</h3>
+
+	<div id="social">
+		<h3>We're Social</h3>
 		<ul>
 			<li><a href="#">Facebook</a></li> 
 			<li><a href="#">Twitter</a></li>
 		</ul>
-		<p>&copy; www.kresko.com 2016</p>
+	</div>
+			
+	<p>&copy; www.kresko.com 2016</p>
 </div>
