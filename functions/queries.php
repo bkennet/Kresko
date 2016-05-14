69333b7af0b56e3f5111e6e17f569fd6bd3519f5
Matthew <mcres@dhcp-gs-1518.redrover.cornell.edu>
2016-05-14 01:05:35 -0400

    added functionality to categories.php, now the page gets displays data from the database.

--- functions/queries.php
+++ functions/queries.php
@@ -1,4 +1,28 @@
 <?php 
 	/* All functions that get data from the database exist here. */
 
+
+	/* Used in category.php */
+
+	function getVendorItems($vendorID) {
+		return "SELECT * FROM `items` 
+			INNER JOIN `vendors` ON 
+			vendors.vendorid = items.vendorid
+			WHERE items.vendorid = $vendorID";
+	}
+
+	function getCategoryItems($categoryID) { 
+		if (is_numeric($categoryID)) {
+			return "SELECT * FROM `items` 
+					INNER JOIN `vendors` ON vendors.vendorid = items.vendorid 
+					WHERE items.catid = $categoryID";
+		} else {
+			return "SELECT * FROM `items` 
+				INNER JOIN `categories` ON categories.catid = items.catid 
+				INNER JOIN `vendors` ON items.vendorid = vendors.vendorid 
+				WHERE categories.category = '$categoryID'";
+		}
+	}	
+
+	/********/
 ?>
