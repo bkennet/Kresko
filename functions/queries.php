2b18d8857498f9aea6cde2b36bff2068b65314a2
bkennet <benjaminkennet@gmail.com>
2016-05-14 01:54:43 -0400

    added queries to get orders

--- functions/queries.php
+++ functions/queries.php
@@ -1,4 +1,44 @@
 <?php 
 	/* All functions that get data from the database exist here. */
-
+	function getallorders(){
+		$query = "SELECT * FROM orders";
+		return $query;
+	}
+	
+	/* to be used only if customer login is implemented*/
+	function getordersfromuser($userid){
+		$query = "SELECT * FROM orders WHERE userid = '$userid';"
+		return $query;
+	}
+	/* query to see if user is a vendor - returns list of vendors associated with user */
+	function isuservendor ($userid){
+		$query = "SELECT * from vendors WHERE vendors.userid = '$userid';";
+		return $query;
+	}
+	
+	/* takes in logged in user, returns query of all orders with items sold by current logged in vendor
+	assumes user is a vendor!
+	*/
+	function getordersbyvendor($loggedinuser){
+		$query = "
+			SELECT DISTINCT orders.orderid, orders.creation_date, orders.address, orders.status
+			FROM (
+			SELECT items.itemid, items.vendorid, itemsinorders.orderid FROM items INNER JOIN itemsinorders on items.itemid = itemsinorders.itemid
+			) as ordernum
+			INNER JOIN orders on ordernum.orderid = orders.orderid
+			INNER JOIN
+			(SELECT vendors.vendorid, vendors.userid FROM vendors INNER JOIN users on vendors.userid = users.userid WHERE vendors.userid = '$loggedinuser' ) as currentvendor
+			ON
+			currentvendor.vendorid = ordernum.vendorid";
+		return $query;
+		
+	}
+	/* return all items associated with an existing order.
+	assumes orderid is valid*/
+	function getorderitems ($orderid){
+		$query = "SELECT items.itemid, items.itemname, items.description, itemsinorders.price, itemsinorders.quantity, items.filepath, categories.category, categories.subcategory
+		FROM itemsinorders INNER JOIN items on itemsinorders.itemid = items.itemid INNER JOIN categories on categories.catid = items.itemid WHERE orderid = '$orderid';"
+		return $query;
+	}
+	
 ?>
