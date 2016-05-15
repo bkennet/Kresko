db65e2154b2c34de6930632bf6984f84fdd341e7
bkennet <benjaminkennet@gmail.com>
2016-05-14 21:01:54 -0400

    adding bag functionality

--- functions/queries.php
+++ functions/queries.php
@@ -78,6 +78,38 @@
 					WHERE items.itemid = $itemID";
 		}
 	}
-
+//cart in session just maps item IDs to quantity. Bag ultimately pulls info from database
+	function addtocart($itemid, $qty){
+		if (isset($_SESSION['cart'])){
+			$cart = $_SESSION['cart'];
+			
+			if (!$cart[$itemid]){
+				$cart[$itemid] = $qty;
+			}
+			else{
+				$cart[$itemid] += $qty;
+			}
+		}
+		else{
+			$cart = array(
+				$itemid: => $qty
+			);
+			$_SESSION['cart'] = $cart;
+		}
+	}
+	function removefromcart($itemid){
+		if (isset($_SESSION['cart'])){
+			//we don't know if itemid is in cart
+			if (isset($_SESSION['cart'][$itemid])){
+				unset($_SESSION['cart'][$itemid])
+			}
+		}
+	}
+	//get item info from db
+	function getiteminfo ($itemid){
+		$query = "SELECT * from items WHERE items.itemid = $itemid;
+		";
+		return $query;
+	}
 	/********/
 ?>
