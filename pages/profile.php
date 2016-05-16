a16f8ff134037e2574e1e97e0f6db69b548df26b
bkennet <benjaminkennet@gmail.com>
2016-05-16 07:56:05 -0400

    changed photo upload mechanism, style

--- pages/profile.php
+++ pages/profile.php
@@ -5,11 +5,12 @@
     <script src = "../includes/validate.js"></script>
     <?php include "../includes/header.php"; ?>
     <?php include "../config.php";?>
+		<?php include "../functions/queries.php"; ?>
   </head>
   <body>
     <div class='container-fluid page-wrapper'> <!-- This will wrap the entire page: allows us to use bootstrap rows and columns -->
 
-      <img id='homepicture' src="../images/background.jpg" alt='hi'>
+      <!--<img id='homepicture' src="../images/background.jpg" alt='hi'>-->
 
       <?php include "../includes/navigation.php"; ?>
 
@@ -22,7 +23,7 @@
           }
           
           // BEGIN CUSTOMER VIEW (AND) BEGIN ADMIN VIEW
-          if (isset($_GET['vendorID']) && ($_SESSION['logged_usertype'] != 2)) {
+          if (isset($_GET['vendorID']) && (isset($_SESSION['logged_usertype']) && $_SESSION['logged_usertype'] != 2)) {
             $id = $_GET['vendorID'];
             $query="SELECT vendorname, description, filepath FROM vendors where vendorid=$id";
             $result=$mysqli->query($query);
@@ -38,7 +39,8 @@
                       </div>
                     </div>");
             }
-          } else { ?>
+          } 
+					else { ?>
             <div class="vendor-edit">
           <?php
             // BEGIN VENDOR VIEW 
@@ -46,7 +48,7 @@
             $query="SELECT vendorname, description, filepath FROM vendors WHERE userid='$userid'";
             $result=$mysqli->query($query);
             while ($row = $result->fetch_assoc()) {
-              print("<img class='vendor-image' src='{$row['filepath']}' alt='artisan-image'/>
+              print("<img class='vendor-image' src='../images/{$row['filepath']}' alt='artisan-image'/>
                     <form action='profile.php' method='post' id='save-profile' enctype='multipart/form-data'>
                     <div class='vendor-info'>
                     <h3 class='white'>{$row['vendorname']}</h3>
@@ -64,9 +66,34 @@
         // VENDOR EDIT FUNCTIONALITY (FOR VENDOR)
         if (isset($_POST['edit_profile'])) {
           $desc = filter_var("{$_POST['descriptionedit']}", FILTER_SANITIZE_STRING);
+					$desc = htmlentities(substr($desc, 0, 400));
+					
           $query="UPDATE vendors SET description='$desc' WHERE userid='$userid'";
           $mysqli->query($query);
-          if (!empty($_FILES['newphoto'])) {
+					if (!empty($_FILES['newphoto'])) {
+							
+								$newPhoto = $_FILES['newphoto'];
+								$newname = $newPhoto['name'];
+								//no upload error?
+								if ($newPhoto['error'] == 0) {
+									$tempName = $newPhoto['tmp_name'];
+									$fname = $newPhoto['name'];
+									$vendorid = getvendorid($_SESSION['logged_userid']);
+									$explode = explode(".", $fname);
+									//get file extension to create name
+									$ext = $explode[1];
+									$filepath = "vendorid-" . $newitemid . "." . $ext;
+									
+									move_uploaded_file( $tempName, "../images/" . $filepath);
+									//print("<p>The file $tempName was uploaded successfully.</p>");
+									$query="UPDATE vendors SET filepath='$location' WHERE userid='$userid'";
+									$mysqli->query($query);
+								} 
+								else {
+									print("<span class='error'>Error: The file was not uploaded.</span>");
+								}
+							}
+          /**if (!empty($_FILES['newphoto'])) {
             $newfile=$_FILES['newphoto'];
             $tempName=$newfile['tmp_name'];
             $name=$newfile['name'];
@@ -74,7 +101,7 @@
             move_uploaded_file($tempName, $location);
             $query="UPDATE vendors SET filepath='$location' WHERE userid='$userid'";
             $mysqli->query($query);
-          }
+          }**/
         }
       ?>
       
