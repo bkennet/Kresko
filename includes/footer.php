cb4659ab8a75cb4109136a38f4099c10584ade15
bkennet <benjaminkennet@gmail.com>
2016-05-16 14:48:49 -0400

    fixed footer formatting

--- includes/footer.php
+++ includes/footer.php
@@ -16,10 +16,7 @@
 
 	<div id="social">
 		<h3>We're Social</h3>
-		<ul>
-			<li><a href="#">Facebook</a></li> 
-			<li><a href="#">Twitter</a></li>
-		</ul>
+		<p>Coming soon: Facebook, Twitter</p>
 	</div>
 			
 	<p id="copyright">&copy; www.kresko.com 2016</p>
